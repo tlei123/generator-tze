31b386c673b358d4eaad8700b5458190e6985fcd
Tze-chiu Lei <tclei2009@gmail.com>
2015-03-06 23:11:41 -0500

    #2 Added testing code.
    Populated /test/test-app.js with real tests.  Refactored index.js and
    package.json.

--- app/index.js
+++ app/index.js
@@ -4,8 +4,12 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
-var tzeGenerator = yeoman.generators.Base.extend({
-    promptUser: function () {
+var TzeGenerator = yeoman.generators.Base.extend({
+    initializing: function () {
+        this.pkg = require('../package.json');
+    },
+
+    prompting: function () {
         var done = this.async();
 
         // Greet User.
@@ -22,7 +26,7 @@ var tzeGenerator = yeoman.generators.Base.extend({
             {
                 name: 'version',
                 message: 'Version?',
-                default: '0.0.3'
+                default: '0.0.4'
             },
             {
                 name: 'author',
@@ -37,7 +41,8 @@ var tzeGenerator = yeoman.generators.Base.extend({
             {
                 type: 'confirm',
                 name: 'eq3optn',
-                message: 'JsHint eqeqeq [require === and !===]?'
+                message: 'JsHint eqeqeq [require === and !===]?',
+                default: true
             }
         ];
 
@@ -60,102 +65,112 @@ var tzeGenerator = yeoman.generators.Base.extend({
         }.bind(this));
     },
 
-    scaffoldFolders: function () {
-        this.mkdir("src");
-        this.mkdir("src/css");
-        this.mkdir("src/fonts");
-        this.mkdir("src/images");
-        this.mkdir("src/sass");
-        this.mkdir("src/sass/modules");
-        this.mkdir("src/sass/partials");
-        this.mkdir("src/sass/libs");
-        this.mkdir("src/js");
-        this.mkdir("src/js/libs");
-        // this.mkdir("src/js/sections");
-        this.mkdir("build");
+    writing: {
+        scaffoldFolders: function () {
+            this.mkdir("src");
+            this.mkdir("src/css");
+            this.mkdir("src/fonts");
+            this.mkdir("src/images");
+            this.mkdir("src/sass");
+            this.mkdir("src/sass/modules");
+            this.mkdir("src/sass/partials");
+            this.mkdir("src/sass/libs");
+            this.mkdir("src/js");
+            this.mkdir("src/js/libs");
+            // this.mkdir("src/js/sections");
+            this.mkdir("build");
 
-        console.log('[scaffoldFolders] Folders generated.');
-    },
+            console.log('[scaffoldFolders] Folders generated.');
+        },
 
-    generatePackageJson: function () {
-        var ctx = {
-            appName: this.appName,
-            version: this.version,
-            author: this.author,
-            email: this.email
-        };
+        generatePackageJson: function () {
+            var ctx = {
+                appName: this.appName,
+                version: this.version,
+                author: this.author,
+                email: this.email
+            };
 
-        this.template('_package.json', 'package.json', ctx);
+            this.template('_package.json', 'package.json', ctx);
 
-        console.log('[generatePackageJson] package.json generated.');
-    },
+            console.log('[generatePackageJson] package.json generated.');
+        },
 
-    copyMainFiles: function () {
-        this.copy("_main.scss", "src/sass/main.scss");
-        this.copy("_reset.scss", "src/sass/libs/_reset.scss");
-        this.copy("_all.scss", "src/sass/modules/_all.scss");
-        this.copy("_vars.scss", "src/sass/modules/_vars.scss");
-        this.copy("_mixins.scss", "src/sass/modules/_mixins.scss");
-        this.copy("_webfonts.scss", "src/sass/modules/_webfonts.scss");
-        this.copy("_base.scss", "src/sass/partials/_base.scss");
-        this.copy("_responsive.scss", "src/sass/partials/_responsive.scss");
-        this.copy("_page.scss", "src/sass/partials/_page.scss");
-        this.copy("_masthead.scss", "src/sass/partials/_masthead.scss");
-        this.copy("_component1.scss", "src/sass/partials/_component1.scss");
-        this.copy("_component2.scss", "src/sass/partials/_component2.scss");
-        this.copy("_jquery.1.7.1.min.js", "src/js/libs/jquery.1.7.1.min.js");
-        this.copy("_jquery-ui.1.8.24.min.js", "src/js/libs/jquery-ui.1.8.24.min.js");
-        this.copy("_modernizr.2.8.3.custom.js", "src/js/libs/modernizr.2.8.3.custom.js");
+        copyMainFiles: function () {
+            this.copy("_main.scss", "src/sass/main.scss");
+            this.copy("_reset.scss", "src/sass/libs/_reset.scss");
+            this.copy("_all.scss", "src/sass/modules/_all.scss");
+            this.copy("_vars.scss", "src/sass/modules/_vars.scss");
+            this.copy("_mixins.scss", "src/sass/modules/_mixins.scss");
+            this.copy("_webfonts.scss", "src/sass/modules/_webfonts.scss");
+            this.copy("_base.scss", "src/sass/partials/_base.scss");
+            this.copy("_responsive.scss", "src/sass/partials/_responsive.scss");
+            this.copy("_page.scss", "src/sass/partials/_page.scss");
+            this.copy("_masthead.scss", "src/sass/partials/_masthead.scss");
+            this.copy("_component1.scss", "src/sass/partials/_component1.scss");
+            this.copy("_component2.scss", "src/sass/partials/_component2.scss");
+            this.copy("_jquery.1.7.1.min.js", "src/js/libs/jquery.1.7.1.min.js");
+            this.copy("_jquery-ui.1.8.24.min.js", "src/js/libs/jquery-ui.1.8.24.min.js");
+            this.copy("_modernizr.2.8.3.custom.js", "src/js/libs/modernizr.2.8.3.custom.js");
 
-        console.log('[copyMainFiles] Main files generated.');
-    },
-    
-    generateHomepage: function () {
-        // Generates index.html with this.appName injected via template.
-        var ctx = {
-            site_name: this.appName
-        };
+            console.log('[copyMainFiles] Main files generated.');
+        },
+        
+        generateHomepage: function () {
+            // Generates index.html with this.appName injected via template.
+            var ctx = {
+                site_name: this.appName
+            };
 
-        this.template('_index.html', 'src/index.html', ctx);
+            this.template('_index.html', 'src/index.html', ctx);
 
-        console.log('[generateHomepage] index.html generated.');
-    },
+            console.log('[generateHomepage] index.html generated.');
+        },
 
-    generateGruntfile: function () {
-        var myOptn = this.eq3optn ? 'true' : 'false';
-        var myCtx = {
-            eqOptn: myOptn
+        generateGruntfile: function () {
+            var myOptn = this.eq3optn ? 'true' : 'false';
+            var myCtx = {
+                eqOptn: myOptn
+            }
+
+            this.template('_gruntfile.js', 'Gruntfile.js', myCtx);
+
+            console.log('[generateGruntFile] Gruntfile.js generated.');
+        },
+
+        generateMainJs: function () {
+            // Generates main.js with this.appName injected via template.
+            var myEquals = this.eq3optn ? '===' : '==';
+            var myNotEquals = this.eq3optn ? '!==' : '!=';
+            var myCtx = {
+                app_name: this.appName,
+                equals: myEquals,
+                not_equals: myNotEquals
+            };
+
+            this.template('_main.js', 'src/js/main.js', myCtx);
+
+            console.log('[generateMainJs] main.js generated.');
         }
 
-        this.template('_gruntfile.js', 'Gruntfile.js', myCtx);
-
-        console.log('[generateGruntFile] Gruntfile.js generated.');
     },
 
-    generateMainJs: function () {
-        // Generates main.js with this.appName injected via template.
-        var myEquals = this.eq3optn ? '===' : '==';
-        var myNotEquals = this.eq3optn ? '!==' : '!=';
-        var myCtx = {
-            app_name: this.appName,
-            equals: myEquals,
-            not_equals: myNotEquals
-        };
-
-        this.template('_main.js', 'src/js/main.js', myCtx);
-
-        console.log('[generateMainJs] main.js generated.');
-    },
-
-    runNpm: function () {
-        // Runs npm install --save-dev based on generated package.json.
-        console.log('[runNpm] Installing npm modules...');
-        this.npmInstall('', function () {
-            console.log('\n[runNpm] Node modules installed.\n\n' + 
-                'YOUR PROJECT IS NOW READY!\n' + 
-                'IMPORTANT: git-/svn-ignore /build, /node_modules, and /.sass-cache folders before commit.\n');
+    install: function () {
+        this.installDependencies({
+            npm: true,
+            bower: false,
+            skipInstall: this.options['skip-install'],
+            callback: function () {
+                console.log('[install] NPM modules installed.');
+            }
         });
+    },
+
+    end: function () {
+        console.log('\nYOUR PROJECT IS NOW READY!\n' + 
+            'IMPORTANT: git-/svn-ignore /build, /node_modules, and /.sass-cache folders before commit.\n');
     }
+
 });
 
-module.exports = tzeGenerator;
+module.exports = TzeGenerator;
--- test/test-app.js
+++ test/test-app.js
@@ -5,23 +5,57 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
 
-describe('tze:app', function () {
+describe('generator-tze test', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
+    this.runGen = helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(os.tmpdir(), './temp-testtze'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
-        someOption: true
+        appName: 'testTze',
+        version: '0.0.4',
+        author: 'Tze Lei',
+        email: 'tze.lei@mrm-mccann.com',
+        eq3optn: false
       })
       .on('end', done);
   });
 
-  it('creates files', function () {
+  it('creates package.json & Gruntfile.js', function () {
     assert.file([
-      'bower.json',
       'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      'Gruntfile.js'
     ]);
   });
+
+  it('creates index.html in src', function () {
+    assert.file([
+      'src/index.html'
+    ]);
+  });
+
+  it('creates JavaScript files in src/js', function () {
+    assert.file([
+      'src/js/main.js',
+      'src/js/libs/jquery.1.7.1.min.js',
+      'src/js/libs/jquery-ui.1.8.24.min.js',
+      'src/js/libs/modernizr.2.8.3.custom.js'
+    ]);
+  });
+
+  it('creates SASS files in src/sass', function () {
+    assert.file([
+      'src/sass/modules/_all.scss',
+      'src/sass/modules/_vars.scss',
+      'src/sass/modules/_mixins.scss',
+      'src/sass/modules/_webfonts.scss',
+      'src/sass/partials/_base.scss',
+      'src/sass/partials/_responsive.scss',
+      'src/sass/partials/_page.scss',
+      'src/sass/partials/_masthead.scss',
+      'src/sass/partials/_component1.scss',
+      'src/sass/partials/_component2.scss',
+      'src/sass/main.scss'
+    ]);
+  });
+
 });
