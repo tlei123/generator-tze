77cfe2eea54358a1e8d87f5013451f79347dbcc1
Tze-chiu Lei <tclei2009@gmail.com>
2015-03-17 22:23:01 -0400

    Bumped to v0.0.10
    Updated package.json, README.md, index.js to v0.0.10.

--- app/index.js
+++ app/index.js
@@ -31,7 +31,7 @@ var TzeGenerator = yeoman.generators.Base.extend({
             {
                 name: 'version',
                 message: 'Version?',
-                default: '0.0.9'
+                default: '0.0.10'
             },
             {
                 name: 'author',
